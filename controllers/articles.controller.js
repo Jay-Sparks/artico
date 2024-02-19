61a328f602b308a7105141ac45d20c00dbcb957f
Jay-Sparks <jay_sparks@icloud.com>
2024-02-19 21:10:11 +0000

    complete get api/articles with all tests passing

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,14 @@
-const { selectArticleById } = require('../models/articles.model')
+const { selectArticles, selectArticleById } = require('../models/articles.model')
+
+exports.getArticles = ( req, res, next ) => {
+    selectArticles()
+        .then((articles) => {
+            res.status(200).send({ articles: articles })
+        })
+        .catch((err) => {
+            next(err)
+        })
+}
 
 exports.getArticleById = (req, res, next) => {
     const articleId = req.params.article_id
--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,15 @@
 const db = require('../db/connection')
 
+exports.selectArticles = () => {
+    return db.query('SELECT * from articles ORDER BY created_at DESC')
+        .then((articles) => {
+            const articleArr = articles.rows.map(({ body, ...article }) => {
+                return article
+            })
+            return articleArr
+        })
+}
+
 exports.selectArticleById = (articleId) => {
     return db.query(`SELECT * FROM articles WHERE article_id = ${articleId}`)
         .then((article) => {
